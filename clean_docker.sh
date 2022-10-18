71e474ae53fee4920248f83995b4413615dc94b3
dognyanoff <danail.ognyanov@linkmobility.com>
2022-10-18 12:15:43 +0300

    Another docker script

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+ #!/bin/bash
 echo -n "What to remove c/i?: "
 echo
 read DELETE
 
 case "$DELETE" in
 i)
-	echo
-	echo -n "Docker image list:"
-	echo
-	docker images
-	echo
-	echo "Which image would you like to delete?: "
-	echo
-	read image_name
-	docker rmi "$image_name"
-	;;
+        echo
+        echo -n "Docker image list:"
+        echo
+        docker images
+        echo
+        echo "Which image would you like to delete?: "
+        echo
+        read image_name
+        docker rmi "$image_name"
+        ;;
 
 c)
-	echo
-	echo -n "Docker container list:"
-	echo
-	docker ps -a
-	read container_name
-	docker rm "$container_name"
-	;;
+        echo
+        echo -n "Docker container list:"
+        echo
+        docker ps -a
+        read container_name
+        docker rm "$container_name"
+        ;;
 
 *)
 echo "Error!!"
